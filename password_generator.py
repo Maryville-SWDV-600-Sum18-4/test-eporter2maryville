7fa58fdd407631ccb498b636fe5864c91610c708
Brent Porter <eporter2@live.maryville.edu>
2018-06-30 16:28:22 -0600

    added main()

--- password_generator.py
+++ password_generator.py
@@ -19,18 +19,21 @@
 
 #The actual code
 from random import *
-passwordLength=int(input("Please enter the length of Password you'd like to create: "))
-caps = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-password = []
-for character in range(passwordLength):
-    lowerUpper = randrange(1,3)#capital letters = 1, lowercase = 2
-    if lowerUpper == 1:
-        character = caps[randrange(0,26)]
-        password.append(character)
-    else:
-        character = lower[randrange(0,26)]
-        password.append(character)
+def main():
+    passwordLength=int(input("Please enter the length of Password you'd like to create: "))
+    caps = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    password = []
+    for character in range(passwordLength):
+        lowerUpper = randrange(1,3)#capital letters = 1, lowercase = 2
+        if lowerUpper == 1:
+            character = caps[randrange(0,26)]
+            password.append(character)
+        else:
+            character = lower[randrange(0,26)]
+            password.append(character)
         
-passwordstr=''.join(password)
-print("Your password is : {0}".format(passwordstr))
+    passwordstr=''.join(password)
+    print("Your password is : {0}".format(passwordstr))
+    
+main()
